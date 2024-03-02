ec7654aaf5a8864e833cea11ffd29bb3c851d859
Kasumi Null <kasumi.null@yandex.com>
2024-03-02 14:55:36 +0800

    move some stuff around, nicer 404

--- website/src/app/not-found.tsx
+++ website/src/app/not-found.tsx
@@ -1,34 +1,28 @@
-import { IconLink } from '@/components/IconLink'
-import { StarField } from '@/components/StarField'
+import { Layout } from '@/components/FullLayout'
+import { Link } from '@/components/Link'
+import Image from 'next/image'
+import BackgroundImage from '@/images/rock-n-roll-monkey.jpg'
 
 export default function NotFound() {
   return (
-    <div className="relative isolate flex flex-auto flex-col items-center justify-center overflow-hidden bg-gray-950 text-center">
-      <svg
-        aria-hidden="true"
-        className="absolute left-1/2 top-[-10vh] -z-10 h-[120vh] w-[120vw] min-w-[60rem] -translate-x-1/2"
-      >
-        <defs>
-          <radialGradient id="gradient" cy="0%">
-            <stop offset="0%" stopColor="rgba(56, 189, 248, 0.3)" />
-            <stop offset="53.95%" stopColor="rgba(0, 71, 255, 0.09)" />
-            <stop offset="100%" stopColor="rgba(10, 14, 23, 0)" />
-          </radialGradient>
-        </defs>
-        <rect width="100%" height="100%" fill="url(#gradient)" />
-      </svg>
-      <StarField className="sm:-mt-16" />
-
-      <p className="font-display text-4xl/tight font-light text-white">404</p>
-      <h1 className="mt-4 font-display text-xl/8 font-semibold text-white">
-        Page not found
-      </h1>
-      <p className="mt-2 text-sm/6 text-gray-300">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p>
-      <IconLink href="/" className="mt-4">
-        Go back home
-      </IconLink>
-    </div>
+    <Layout>
+      <main className="relative isolate min-h-full">
+        <Image
+          src={BackgroundImage}
+          alt="lost robot"
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-top blur-sm"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+          <p className="text-base font-semibold leading-8 text-gray">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray sm:text-5xl">Page not found</h1>
+          <p className="mt-4 text-base text-gray/70 sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex justify-center">
+            <Link href="/" className="text-sm font-semibold leading-7 text-gray">
+              <span aria-hidden="true">&larr;</span> Back to home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </Layout>
   )
 }
