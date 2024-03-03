35fa33fd1a3f9e48a3e2a6198765fee785d710fc
Kasumi Null <kasumi.null@yandex.com>
2024-03-03 09:13:15 +0800

    import unicrow contracts for testing

---
+++ contract/contracts/unicrow/FakeToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title FakeToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract FakeToken is ERC20 {
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 300000_000 * 10 ** decimals());
+    }
+}
