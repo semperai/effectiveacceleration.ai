f5bfc7eaccdb5e5e2359f7b1e1e1f23f2c63da06
Kasumi Null <kasumi.null@yandex.com>
2024-03-03 23:09:35 +0800

    styling

--- website/src/components/TokenButton.tsx
+++ website/src/components/TokenButton.tsx
@@ -2,30 +2,29 @@ import { Avatar } from '@/components/Avatar'
 import { Token } from '@/tokens'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-const baseClass = "w-fit rounded-full font-medium text-xl flex gap-2 px-3 py-2 items-center"
+const baseClass = "w-fit rounded-lg font-medium flex gap-2 items-center"
 
-export interface ISelect {
+
+export function TokenButton({ onClick, selectedToken }: {
   onClick: () => void
   selectedToken: Token | undefined
-}
-
-export function TokenButton({ onClick, selectedToken }: ISelect) {
+}) {
   if (selectedToken) {
     return (
       <button
         onClick={onClick}
-        className={`${baseClass} bg-slate-200 hover:bg-slate-300 text-slate-900`}
+        className={`${baseClass} bg-slate-100 hover:bg-slate-200 text-slate-900`}
       >
         <Avatar className="size-8" src={selectedToken.icon} />
         <div>{selectedToken.symbol}</div>
-        <ChevronDownIcon className="h-6 w-6 text-white" aria-hidden="true" />
+        <ChevronDownIcon className="h-6 w-6 text-black" aria-hidden="true" />
       </button>
     )
   } else {
     return (
       <button
         onClick={onClick}
-        className={`${baseClass} bg-rose-500 hover:bg-rose-600 text-white`}
+        className={`${baseClass} bg-blue-500 hover:bg-blue-600 text-white`}
       >
         <div>Select token</div>
         <ChevronDownIcon className="h-6 w-6 text-white" aria-hidden="true" />
