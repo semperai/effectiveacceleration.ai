d02e53b57bacc83771bdf6c52d94ca323488e399
Kasumi Null <kasumi.null@yandex.com>
2024-03-04 06:10:55 +0800

    fix changelog styling

--- website/src/components/Mdx.tsx
+++ website/src/components/Mdx.tsx
@@ -29,11 +29,11 @@ function ContentWrapper({
   ...props
 }: React.ComponentPropsWithoutRef<'div'>) {
   return (
-    <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
-      <div className="lg:ml-96 lg:flex lg:w-full lg:justify-end lg:pl-32">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="">
         <div
           className={clsx(
-            'mx-auto max-w-lg lg:mx-0 lg:w-0 lg:max-w-xl lg:flex-auto',
+            'mx-auto max-w-lg',
             className,
           )}
           {...props}
@@ -46,14 +46,13 @@ function ContentWrapper({
 function ArticleHeader({ id, date }: { id: string; date: string | Date }) {
   return (
     <header className="relative mb-10 xl:mb-0">
-      <div className="pointer-events-none absolute left-[max(-0.5rem,calc(50%-18.625rem))] top-0 z-50 flex h-4 items-center justify-end gap-x-2 lg:left-0 lg:right-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] xl:h-8">
+      <div className="pointer-events-none absolute z-50 flex h-4 items-center gap-x-2">
         <Link href={`#${id}`} className="inline-flex">
           <FormattedDate
             date={date}
             className="hidden xl:pointer-events-auto xl:block xl:text-2xs/4 xl:font-medium xl:text-white/50"
           />
         </Link>
-        <div className="h-[0.0625rem] w-3.5 bg-gray-400 lg:-mr-3.5 xl:mr-0 xl:bg-gray-300" />
       </div>
       <ContentWrapper>
         <div className="flex">
