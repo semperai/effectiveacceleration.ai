fc09dd9a5c5f321bbf8a0855f8ea36b353ac10b8
Kasumi Null <kasumi.null@yandex.com>
2024-03-05 06:21:19 +0800

    add size method back

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
