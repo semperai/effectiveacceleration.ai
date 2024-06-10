c56784caef65785596a26e599f01a3124f681a37
mainnet-pat <mainnet_pat@protonmail.com>
2024-06-10 13:29:32 +0000

    Add ecnryption utils and tests

--- contract/src/utils.ts
+++ contract/src/utils.ts
@@ -1,4 +1,122 @@
-import { BytesLike, getBytes, AbiCoder, hexlify, toUtf8String, toBigInt, getAddress } from "ethers";
+import { xchacha20poly1305 } from "@noble/ciphers/chacha";
+import { randomBytes } from "@noble/ciphers/crypto";
+import { utf8ToBytes } from "@noble/ciphers/utils";
+import { BytesLike, getBytes, AbiCoder, hexlify, toUtf8String, toBigInt, getAddress, decodeBase58, encodeBase58, toBeArray, encodeBase64, decodeBase64, Signer, SigningKey, keccak256 } from "ethers";
+
+export const cidToHash = (cid: string): string => {
+  if (cid.length !== 46 || !cid.match(/^Qm/)) {
+    throw new Error("invalid cid");
+  }
+  return hexlify(toBeArray(decodeBase58(cid)).slice(2));
+}
+
+export const hashToCid = (hash: string): string => {
+  if (getBytes(hash).length !== 32) {
+    throw new Error("invalid hash");
+  }
+  return encodeBase58(Uint8Array.from([0x12, 0x20, ...getBytes(hash)]));
+}
+
+export const encryptBinaryData = (data: Uint8Array, sessionKey: string | undefined): Uint8Array => {
+  if (data.length === 0) {
+    throw new Error("empty data");
+  }
+
+  if (!sessionKey) {
+    return Uint8Array.from([...new Uint8Array(24), ...data]);
+  }
+
+  const nonce = randomBytes(24);
+  const chacha = xchacha20poly1305(getBytes(sessionKey), nonce);
+  return Uint8Array.from([...nonce, ...chacha.encrypt(data)]);
+}
+
+export const encryptUtf8Data = (data: string, sessionKey: string | undefined): Uint8Array => {
+  return encryptBinaryData(utf8ToBytes(data), sessionKey);
+}
+
+export const publishToIpfs = async (message: string, sessionKey: string | undefined = undefined): Promise<{
+  hash: string;
+  cid: string;
+}> => {
+  const encodedData: string = encodeBase64(encryptUtf8Data(message, sessionKey));
+
+  const host = process.env.IPFS_API_URL || "http://localhost:8000";
+
+  const response = await fetch(host, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      dataB64: encodedData,
+    }),
+  });
+
+  const { hash, cid } = await response.json();
+  return { hash, cid };
+}
+
+const decryptBinaryData = (data: Uint8Array, sessionKey: string | undefined): Uint8Array => {
+  const nonce = data.slice(0, 24);
+  if (nonce.every(b => b === 0)) {
+    return data.slice(24);
+  }
+
+  if (!sessionKey) {
+    throw new Error("Session key is required to decrypt the message");
+  }
+
+  const chacha = xchacha20poly1305(getBytes(sessionKey), nonce);
+  return chacha.decrypt(data.slice(24));
+}
+
+const decryptUtf8Data = (data: Uint8Array, sessionKey: string | undefined): string => {
+  return toUtf8String(decryptBinaryData(data, sessionKey));
+}
+
+export const getFromIpfs = async (cidOrHash: string, sessionKey: string | undefined = undefined): Promise<string> => {
+  const isCid = cidOrHash.indexOf("Qm") === 0;
+  if (!isCid) {
+    cidOrHash = hashToCid(cidOrHash);
+  }
+
+  const host = process.env.IPFS_GW_URL || "http://localhost:8080";
+  const respose = await fetch(`${host}/ipfs/${cidOrHash}`);
+  if (respose.status !== 200) {
+    throw new Error(`Failed to fetch data from IPFS\n${respose.statusText}\n${await respose.text()}`);
+  }
+  const dataB64 = await respose.text();
+
+  const decodedData = decodeBase64(dataB64);
+  return decryptUtf8Data(decodedData, sessionKey);
+}
+
+export const getEncryptionSigningKey = async (signer: Signer): Promise<SigningKey> => {
+  const key = `HashedSignature-${await signer.getAddress()}`;
+
+  let hashedSignature = globalThis.localStorage?.getItem(key);
+  if (hashedSignature) {
+    return new SigningKey(hashedSignature);
+  }
+
+  const signature = await signer.signMessage("MarketplaceV1");
+  hashedSignature = keccak256(keccak256(signature));
+  globalThis.localStorage?.setItem(key, hashedSignature);
+  return new SigningKey(hashedSignature);
+}
+
+export const getSessionKey = async (signer: Signer, otherCompressedPublicKey: string): Promise<string> => {
+  if (getBytes(otherCompressedPublicKey).length !== 33) {
+    throw new Error("Invalid public key, must be compressed");
+  }
+
+  const aliceEncryptionSigningKey = await getEncryptionSigningKey(signer);
+
+  const sharedSecret = aliceEncryptionSigningKey.computeSharedSecret(otherCompressedPublicKey);
+
+  return keccak256(keccak256(sharedSecret));
+}
 
 export enum JobState {
   Open = 0,
@@ -61,9 +179,15 @@ export type JobRatedEvent = {
   review: string;
 }
 
-export type JobDisputedEvent = {
+export type JobDisputedEventRaw = {
   sessionKey: string; // Creator's and worker's session key, encrypted for arbitrator
   content: string; // Dispute content encrypted by contender + arbitrator shared secret
+}
+
+// Same as JobDisputedEventRaw, but with values decrypted using the contender + arbitrator shared secret
+export type JobDisputedEvent = {
+  sessionKey: string; // Creator's and worker's session key
+  content: string; // Dispute content
 }
 
 export type JobArbitratedEvent = {
@@ -71,7 +195,7 @@ export type JobArbitratedEvent = {
   creatorAmount: bigint;
   workerShare: number;
   workerAmount: bigint;
-  reason: string;
+  reasonHash: string;
 }
 
 export const decodeJobPostEvent = (rawData: BytesLike): JobPostEvent => {
@@ -125,12 +249,26 @@ export const decodeJobRatedEvent = (rawData: BytesLike): JobRatedEvent => {
   };
 };
 
-export const decodeJobDisputedEvent = (rawData: BytesLike): JobDisputedEvent => {
+export const decodeJobDisputedEventRaw = (rawData: BytesLike): JobDisputedEventRaw => {
   const bytes = getBytes(rawData);
-  return {
-    sessionKey: hexlify(bytes.slice(0, 32)),
-    content: hexlify(bytes.slice(32)),
-  };
+  let ptr = {bytes, index: 0};
+
+  const result = {} as JobDisputedEvent;
+  result.sessionKey = decodeBytes(ptr);
+  result.content = decodeBytes(ptr);
+
+  return result;
+}
+
+export const decodeJobDisputedEvent = (rawData: BytesLike, arbitratorSessionKey: string): JobDisputedEvent => {
+  const bytes = getBytes(rawData);
+  let ptr = {bytes, index: 0};
+
+  const result = {} as JobDisputedEvent;
+  result.sessionKey = hexlify(decryptBinaryData(getBytes(decodeBytes(ptr)), arbitratorSessionKey));
+  result.content = decryptUtf8Data(getBytes(decodeBytes(ptr)), arbitratorSessionKey);
+
+  return result;
 }
 
 export const decodeJobArbitratedEvent = (rawData: BytesLike): JobArbitratedEvent => {
@@ -141,7 +279,7 @@ export const decodeJobArbitratedEvent = (rawData: BytesLike): JobArbitratedEvent
     creatorAmount: toBigInt(bytes.slice(2, 34)),
     workerShare: dataView.getUint16(34),
     workerAmount: toBigInt(bytes.slice(36, 68)),
-    reason: hexlify(bytes.slice(68)),
+    reasonHash: hexlify(bytes.slice(68)),
   };
 }
 
@@ -150,6 +288,15 @@ const decodeString = (ptr: {bytes: Uint8Array, index: number}): string => {
   const length = ptr.bytes[ptr.index];
   ptr.index++;
   const result = toUtf8String(ptr.bytes.slice(ptr.index, ptr.index + length));
+  ptr.index += length;
+
+  return result;
+}
+
+const decodeBytes = (ptr: {bytes: Uint8Array, index: number}): string => {
+  const length = ptr.bytes[ptr.index];
+  ptr.index++;
+  const result = hexlify(ptr.bytes.slice(ptr.index, ptr.index + length));
   ptr.index += length;
 
   return result;
---
+++ contract/test/utils.test.ts
@@ -0,0 +1,92 @@
+import { ethers } from "hardhat";
+import {hashToCid, cidToHash, getFromIpfs, publishToIpfs, getEncryptionSigningKey, getSessionKey} from "../src/utils";
+import { expect } from "chai";
+import { ZeroHash } from "ethers";
+
+describe("utils", () => {
+  describe("hashToCid", () => {
+    it("should convert a hash to a cid", () => {
+      const hash = "0x31479ede5414b18e9205f0ab03b8cfac02f4c449ae9a993fc91c9b88c84efbcc";
+      const cid = hashToCid(hash);
+      expect(cid).equal("QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm");
+    });
+
+    it("should throw an error if hash is not a valid hex", () => {
+      expect(() => hashToCid("0x1234")).to.throw("invalid hash");
+    });
+  });
+
+  describe("cidToHash", () => {
+    it("should convert a cid to a hash", () => {
+      const cid = "QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm";
+      const hash = cidToHash(cid);
+      expect(hash).equal("0x31479ede5414b18e9205f0ab03b8cfac02f4c449ae9a993fc91c9b88c84efbcc");
+    });
+
+    it("should throw an error if cid is empty", () => {
+      expect(() => cidToHash("")).to.throw("invalid cid");
+    });
+
+    it("should throw an error if cid is not a string", () => {
+      expect(() => cidToHash(123 as any)).to.throw("invalid cid");
+    });
+
+    it("should throw an error if cid is not a valid base58", () => {
+      expect(() => cidToHash("QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm=")).to.throw("invalid cid");
+    });
+  });
+
+  let encryptedCid = "";
+  describe("publishToIpfs", () => {
+    it("should publish an unencrypted message to ipfs", async () => {
+      const { hash, cid } = await publishToIpfs("unencrypted", undefined);
+      expect(hash).to.equal("0x1a8fd29da207cbd2438153d9871fd3a8dd2e6345d385f519ad149867fdbdb61d");
+      expect(cid).to.equal("QmQ8LWNYRHdz3JEnA12A2KkpKBwEbcpmzWfGDWj3KMapwW");
+    });
+
+    it("should publish an encrypted message to ipfs", async () => {
+      const { hash, cid } = await publishToIpfs("encrypted", ZeroHash);
+      expect(hash.length).to.equal(66);
+      expect(cid).to.match(/^Qm/);
+      encryptedCid = cid;
+    });
+
+    it("should throw an error if message is empty", async () => {
+      await expect(publishToIpfs("", undefined)).to.be.rejectedWith("empty data");
+    });
+  });
+
+  describe("getFromIpfs", () => {
+    it("should get an unencrypted message from ipfs", async () => {
+      const hash = "QmQ8LWNYRHdz3JEnA12A2KkpKBwEbcpmzWfGDWj3KMapwW";
+      const message = await getFromIpfs(hash, undefined);
+      expect(message).to.equal("unencrypted");
+    });
+
+    it("should get an encrypted message from ipfs", async () => {
+      const message = await getFromIpfs(encryptedCid, ZeroHash);
+      expect(message).to.equal("encrypted");
+    });
+  });
+
+  describe("getEncryptionSigningKey", () => {
+    it("should get an encryption signing key", async () => {
+      const [signer] = await ethers.getSigners();
+      const key = await getEncryptionSigningKey(signer);
+      expect(key).to.be.an("object");
+    });
+  });
+
+  describe("getSessionKey", () => {
+    it("should get a session key", async () => {
+      const [signer, other] = await ethers.getSigners();
+
+      const signerSigningKey = await getEncryptionSigningKey(signer);
+      const otherSigningKey = await getEncryptionSigningKey(other);
+
+      const sessionKey = await getSessionKey(signer, otherSigningKey.compressedPublicKey);
+      const otherSessionKey = await getSessionKey(other, signerSigningKey.compressedPublicKey);
+      expect(sessionKey).equal(otherSessionKey);
+    });
+  });
+});
