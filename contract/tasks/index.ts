a1e2704bbab518ecdde3e2b3d14766fb008d6f05
mainnet-pat <mainnet_pat@protonmail.com>
2024-06-11 14:04:59 +0000

    Add contract seeding for local environment

--- contract/tasks/index.ts
+++ contract/tasks/index.ts
@@ -5,6 +5,8 @@ import { FakeToken } from '../typechain-types/contracts/unicrow/FakeToken';
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { task } from "hardhat/config";
 import Config from '../scripts/config.json';
+import { getEncryptionSigningKey, publishToIpfs } from '../src/utils';
+import { ZeroAddress } from 'ethers';
 
 
 async function getMinerAddress(hre: HardhatRuntimeEnvironment) {
@@ -33,91 +35,173 @@ task("timetravel", "go into the future")
   await hre.network.provider.send("evm_mine");
 });
 
-task("gen-wallet", "Creates a new wallet", async (taskArgs, hre) => {
-  const wallet = hre.ethers.Wallet.createRandom();
-  console.log('address', wallet.address);
-  console.log('mnemonic', wallet.mnemonic.phrase);
-  console.log('privateKey', wallet.privateKey);
-});
+// task("gen-wallet", "Creates a new wallet", async (taskArgs, hre) => {
+//   const wallet = hre.ethers.Wallet.createRandom();
+//   console.log('address', wallet.address);
+//   console.log('mnemonic', wallet.mnemonic.phrase);
+//   console.log('privateKey', wallet.privateKey);
+// });
 
-task("send-eth", "Send ether")
-.addParam("to", "Receiver address")
-.addParam("amount", "Eth")
-.setAction(async ({ to, amount }, hre) => {
-  const [account] = await hre.ethers.getSigners();
-  console.log(`Sending ${amount} ETH to ${to}`);
-  const receipt = await account.sendTransaction({
-    to,
-    value: hre.ethers.parseEther(amount),
-  });
-  console.log(`Transaction hash: ${receipt.hash}`);
-});
+// task("send-eth", "Send ether")
+// .addParam("to", "Receiver address")
+// .addParam("amount", "Eth")
+// .setAction(async ({ to, amount }, hre) => {
+//   const [account] = await hre.ethers.getSigners();
+//   console.log(`Sending ${amount} ETH to ${to}`);
+//   const receipt = await account.sendTransaction({
+//     to,
+//     value: hre.ethers.parseEther(amount),
+//   });
+//   console.log(`Transaction hash: ${receipt.hash}`);
+// });
 
-task("send-token", "Mint test tokens")
-.addParam("to", "address")
-.addParam("amount", "amount")
-.setAction(async ({ to, amount }, hre) => {
-    console.log(`Sending ${amount} TST to ${to}`);
-    const Token = await hre.ethers.getContractFactory("FakeToken");
-    const fakeToken = await Token.attach(Config.fakeTokenAddress) as FakeToken;
-    const tx = await fakeToken.transfer(to, hre.ethers.parseEther(amount));
-    const receipt = await tx.wait();
-    console.log(`Transaction hash: ${receipt.hash}`);
-});
+// task("send-token", "Mint test tokens")
+// .addParam("to", "address")
+// .addParam("amount", "amount")
+// .setAction(async ({ to, amount }, hre) => {
+//     console.log(`Sending ${amount} TST to ${to}`);
+//     const Token = await hre.ethers.getContractFactory("FakeToken");
+//     const fakeToken = await Token.attach(Config.fakeTokenAddress) as FakeToken;
+//     const tx = await fakeToken.transfer(to, hre.ethers.parseEther(amount));
+//     const receipt = await tx.wait();
+//     console.log(`Transaction hash: ${receipt.hash}`);
+// });
 
 
-task("marketplace:transferOwnership", "Transfer admin ownership of Marketplace")
-.addParam("address", "To who?")
-.setAction(async ({ address }, hre) => {
-  const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
-  const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
-  const tx = await marketplace.transferOwnership(address);
-  await tx.wait();
-});
+// task("marketplace:transferOwnership", "Transfer admin ownership of Marketplace")
+// .addParam("address", "To who?")
+// .setAction(async ({ address }, hre) => {
+//   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
+//   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
+//   const tx = await marketplace.transferOwnership(address);
+//   await tx.wait();
+// });
 
-task("marketplace:transferPauser", "Transfer pause ability of Marketplace")
-.addParam("address", "To who?")
-.setAction(async ({ address }, hre) => {
-  const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
-  const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
-  const tx = await marketplace.transferPauser(address);
-  await tx.wait();
-});
+// task("marketplace:transferPauser", "Transfer pause ability of Marketplace")
+// .addParam("address", "To who?")
+// .setAction(async ({ address }, hre) => {
+//   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
+//   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
+//   const tx = await marketplace.transferPauser(address);
+//   await tx.wait();
+// });
 
-task("marketplace:isPaused", "Check if marketplace is paused")
+// task("marketplace:isPaused", "Check if marketplace is paused")
+// .setAction(async ({ }, hre) => {
+//   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
+//   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
+//   const paused = await marketplace.paused();
+//   console.log(`Marketplace is paused: ${paused}`);
+// });
+
+// task("marketplace:setPaused", "Pause engine")
+// .addParam("pause", "Pause/Unpause")
+// .setAction(async ({ pause }, hre) => {
+//   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
+//   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
+//   const tx = await marketplace.setPaused(pause === 'true');
+//   await tx.wait();
+//   const paused = await marketplace.paused();
+//   console.log(`Marketplace is now ${paused ? 'paused' : 'unpaused'}`);
+// });
+
+// task("marketplace:setVersion", "Set engine version for miner check")
+// .addParam("n", "Version Number")
+// .setAction(async ({ n }, hre) => {
+//   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
+//   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
+//   const tx = await marketplace.setVersion(n);
+//   await tx.wait();
+//   const versionNow = await marketplace.version();
+//   console.log(`Marketplace is now version ${versionNow}`);
+// });
+
+// task("marketplace:version", "Set engine version for miner check")
+// .setAction(async ({ }, hre) => {
+//   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
+//   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
+//   const versionNow = await marketplace.version();
+//   console.log(`Marketplace is now version ${versionNow}`);
+// });
+
+task("marketplace:seed", "Seed local marketplace instance")
 .setAction(async ({ }, hre) => {
   const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
   const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
-  const paused = await marketplace.paused();
-  console.log(`Marketplace is paused: ${paused}`);
-});
 
-task("marketplace:setPaused", "Pause engine")
-.addParam("pause", "Pause/Unpause")
-.setAction(async ({ pause }, hre) => {
-  const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
-  const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
-  const tx = await marketplace.setPaused(pause === 'true');
-  await tx.wait();
-  const paused = await marketplace.paused();
-  console.log(`Marketplace is now ${paused ? 'paused' : 'unpaused'}`);
-});
+  const FakeToken = await hre.ethers.getContractFactory("FakeToken");
+  const fakeToken = await FakeToken.attach(Config.fakeTokenAddress) as FakeToken;
 
-task("marketplace:setVersion", "Set engine version for miner check")
-.addParam("n", "Version Number")
-.setAction(async ({ n }, hre) => {
-  const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
-  const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
-  const tx = await marketplace.setVersion(n);
-  await tx.wait();
-  const versionNow = await marketplace.version();
-  console.log(`Marketplace is now version ${versionNow}`);
-});
+  const [deployer, user1, user2] = await hre.ethers.getSigners();
 
-task("marketplace:version", "Set engine version for miner check")
-.setAction(async ({ }, hre) => {
-  const Marketplace = await hre.ethers.getContractFactory("MarketplaceV1");
-  const marketplace = await Marketplace.attach(Config.marketplaceAddress) as Marketplace;
-  const versionNow = await marketplace.version();
-  console.log(`Marketplace is now version ${versionNow}`);
+  console.log('Seeding marketplace with test data');
+
+  console.log('Sending fake tokens to users');
+  await fakeToken.connect(deployer).transfer(await deployer.getAddress(), BigInt(1000e18));
+  await fakeToken.connect(deployer).approve(await marketplace.getAddress(), BigInt(2000e18));
+
+  await fakeToken.connect(deployer).transfer(await user1.getAddress(), BigInt(1000e18));
+  await fakeToken.connect(user1).approve(await marketplace.getAddress(), BigInt(2000e18));
+
+  await fakeToken.connect(deployer).transfer(await user2.getAddress(), BigInt(1000e18));
+  await fakeToken.connect(user2).approve(await marketplace.getAddress(), BigInt(2000e18));
+
+  console.log("Registering users");
+  await marketplace.connect(deployer).registerPublicKey((await getEncryptionSigningKey(deployer)).compressedPublicKey);
+  await marketplace.connect(user1).registerPublicKey((await getEncryptionSigningKey(user1)).compressedPublicKey);
+  await marketplace.connect(user2).registerPublicKey((await getEncryptionSigningKey(user2)).compressedPublicKey);
+
+  console.log('Creating jobs');
+  {
+    const content = 'I am looking for someone to write a story book about Bitcoin and the blockchain. The book should be at least 20 pages long and include a brief history of Bitcoin, the technology behind it, and the potential impact it could have on the world. I am looking for someone who can write in a way that is easy to understand and engaging for a general audience. It should have illustrations and be suitable for children.';
+    const { hash: contentHash } = await publishToIpfs(content);
+    await marketplace.connect(deployer).publishJobPost(
+      'Create a story book about Bitcoin',
+      contentHash,
+      true,
+      ["DT"],
+      await fakeToken.getAddress(),
+      BigInt(100e18),
+      BigInt(60 * 60 * 24 * 7),
+      "Digital",
+      ZeroAddress,
+      []
+    );
+  }
+
+  {
+    const content = 'I am looking to compose a song about Bitcoin. The song should be catchy and easy to remember. It should include lyrics that explain what Bitcoin is, how it works, and why it is important. The song should be suitable for all ages and should be fun to listen to. I am looking for someone who can write lyrics and compose music.';
+    const { hash: contentHash } = await publishToIpfs(content);
+    await marketplace.connect(deployer).publishJobPost(
+      'Create a music track about Bitcoin',
+      contentHash,
+      true,
+      ["DA"],
+      await fakeToken.getAddress(),
+      BigInt(100e18),
+      BigInt(60 * 60 * 24 * 7),
+      "Digital",
+      ZeroAddress,
+      []
+    );
+  }
+
+  {
+    const content = 'I am looking for someone to design a landing page for a new token. The landing page should be visually appealing and easy to navigate. It should include information about the token, its use cases, and how to purchase it. I am looking for someone who can create a design that is modern and professional.';
+    const { hash: contentHash } = await publishToIpfs(content);
+    await marketplace.connect(deployer).publishJobPost(
+      'Design a landing page for a new token',
+      contentHash,
+      true,
+      ["DA"],
+      await fakeToken.getAddress(),
+      BigInt(100e18),
+      BigInt(60 * 60 * 24 * 7),
+      "Digital",
+      ZeroAddress,
+      []
+    );
+  }
+
+  console.log("Done seeding marketplace");
 });
