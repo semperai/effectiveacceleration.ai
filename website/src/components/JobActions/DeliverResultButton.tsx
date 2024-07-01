eb156bad3f85770a0dfd11d6791b526519d46781
mainnet-pat <mainnet_pat@protonmail.com>
2024-07-01 18:17:29 +0000

    Rework deliver result button to use modals

--- website/src/components/JobActions/DeliverResultButton.tsx
+++ website/src/components/JobActions/DeliverResultButton.tsx
@@ -1,21 +1,22 @@
 import { Button } from '@/components/Button'
 import { CheckIcon } from "@heroicons/react/20/solid";
-import { Job, JobEventWithDiffs, publishToIpfs } from "effectiveacceleration-contracts";
+import { Job, publishToIpfs } from "effectiveacceleration-contracts";
 import { MARKETPLACE_V1_ABI } from "effectiveacceleration-contracts/wagmi/MarketplaceV1";
 import Config from "effectiveacceleration-contracts/scripts/config.json";
 import { useEffect, useState } from "react";
-import { zeroAddress } from "viem";
 import { useWaitForTransactionReceipt, useWriteContract } from "wagmi";
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { Textarea } from '../Textarea';
 
 export type DeliverResultButtonProps = {
   address: `0x${string}` | undefined,
-  events: JobEventWithDiffs[],
   sessionKeys: Record<string, string>,
   job: Job | undefined,
-  message: string,
 }
 
-export function DeliverResultButton({address, job, events, message, sessionKeys, ...rest}: DeliverResultButtonProps & React.ComponentPropsWithoutRef<'div'>) {
+export function DeliverResultButton({address, job, sessionKeys, ...rest}: DeliverResultButtonProps & React.ComponentPropsWithoutRef<'div'>) {
+  const [message, setMessage] = useState<string>("");
   const {
     data: hash,
     error,
@@ -41,6 +42,7 @@ export function DeliverResultButton({address, job, events, message, sessionKeys,
         }
       }
       setButtonDisabled(false);
+      closeModal();
     }
   }, [isConfirmed, error]);
 
@@ -68,12 +70,68 @@ export function DeliverResultButton({address, job, events, message, sessionKeys,
     });
   }
 
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
   return (job?.roles.worker === address) ? (<>
     <span className="ml-3">
-      <Button disabled={buttonDisabled} onClick={buttonClick}>
+      <Button disabled={buttonDisabled} onClick={() => openModal()}>
         <CheckIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
         Deliver Result
       </Button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Deliver Result
+                  </Dialog.Title>
+                  <div className='mt-5 mb-3 flex flex-col gap-5'>
+                    <Textarea rows={4} value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" className="mt-5" />
+                    <Button disabled={buttonDisabled} onClick={buttonClick}>
+                      <CheckIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                      Confirm
+                    </Button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </span>
   </>) : <></>
 }
