2a38d2f14398af5683927787b013985631ebbb61
unknown <jorgevillacreces1@gmail.com>
2024-07-01 07:27:58 -0400

    Fixed combobox styling

--- website/src/components/ComboBox.tsx
+++ website/src/components/ComboBox.tsx
@@ -48,15 +48,16 @@ export function Combobox<T>({ options, value, onChange, className, ...props }: C
         <ComboboxOptions
           anchor={{to: "bottom"}}
           className={clsx(
-            'w-[var(--input-width)] rounded-xl border border-white/5 bg-white/5 p-1 [--anchor-gap:var(--spacing-1)] empty:invisible',
-            'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
+            'w-[var(--input-width)] rounded-xl border border-white/5  p-1 [--anchor-gap:var(--spacing-1)] empty:invisible',
+            'transition duration-100 ease-in bg-black',
+            className
           )}
         >
           {filteredOptions.map((option) => (
             <ComboboxOption
               key={option.id}
               value={option}
-              className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
+              className="group flex items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]: cursor-pointer hover:bg-white hover:bg-opacity-20"
             >
               <CheckIcon className="invisible size-4 fill-white group-data-[selected]:visible" />
               <div className="text-sm/6 text-white">{option.name}</div>
