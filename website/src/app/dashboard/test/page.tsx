4a5a49d54a869635ffcb46d1f12363bfbc37dd7f
mainnet-pat <mainnet_pat@protonmail.com>
2024-07-04 04:15:44 +0000

    Cherrypick combobox styling

--- website/src/app/dashboard/test/page.tsx
+++ website/src/app/dashboard/test/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Combobox } from "@/components/ComboBox";
+import { ComboBox } from "@/components/ComboBox";
 import { Layout } from "@/components/Dashboard/Layout";
 import useUsers from "@/hooks/useUsers";
 
@@ -8,8 +8,8 @@ export default function Test() {
   const {data: users} = useUsers();
   const options = users?.map(user => ({id: user.address_ as  string, name: user.name})) ?? [];
   return <Layout>
-    <Combobox className="w-auto border border-gray-300 rounded-md shadow-sm bg-slate-600" options={options} value={options[0]} onChange={(option) => {
+    <ComboBox className="w-auto border border-gray-300 rounded-md shadow-sm bg-slate-600" options={options} value={options[0]} onChange={(option) => {
     console.log(option);
-  }}></Combobox>
+  }}></ComboBox>
   </Layout>
 }
--- website/src/components/ComboBox.tsx
+++ website/src/components/ComboBox.tsx
@@ -5,27 +5,39 @@ import clsx from 'clsx'
 import {useEffect, useRef, useState } from 'react'
 import useDimensions from '@/hooks/useDimensions'
 
-export type Option = {
+export type ComboboxProps<T> = {
+  value?: T,
+  options: ComboBoxOption[],
+  onChange: (value: T) => void,
+  placeholder?: string, 
+}
+
+export type ComboBoxOption = {
   id: number | string,
   name: string,
 }
 
-export type ComboboxProps<T> = {
-  value: T,
-  options: Option[],
-  onChange: (value: T) => void,
-}
 
-export function Combobox<T>({ options, value, onChange, className, ...props }: ComboboxProps<T> & React.ComponentPropsWithoutRef<'div'>) {
+export function ComboBox<T>({ options, value, onChange, className, placeholder, ...props }: ComboboxProps<T> & React.ComponentPropsWithoutRef<'div'>) {
   const inputRef = useRef(null)
+  const dropdownRef = useRef<HTMLUListElement>(null)
   const {width, height} = useDimensions(inputRef)
   const [query, setQuery] = useState('')
   const [selected, setSelected] = useState(value)
 
+
   // Tailwind JIT doesn't support injecting CSS variables dynamically, 
   // styles={} didn't work on ComboboxOptions either, so we're using this workaround
+  function changeWidthDropdown() {
+      setTimeout(function () {
+        if (!dropdownRef.current) return
+        dropdownRef.current.style.width = `${width}px`;
+        dropdownRef.current.style.setProperty('width', `${width}px`, 'important');
+      }, 1)
+  }
+
   useEffect(() => {
-    document.documentElement.style.setProperty('--combobox-dropdown-width', `${width}px`);
+    changeWidthDropdown()
   }, [width]);
 
   const filteredOptions =
@@ -37,37 +49,56 @@ export function Combobox<T>({ options, value, onChange, className, ...props }: C
 
   return (
     <>
-      <style>
-        { `
-          :root {
-            --combobox-dropdown-width: ${width}px;
-          }
-        `}
-      </style>
-      <div className={className}>
+      <div>
         <HeadlessCombobox value={selected} onChange={(value) => {
             setSelected(value)
             onChange(value)
-          }} __demoMode>
-          <div className="relative" ref={inputRef}>
+          }}>
+          <div className="relative">
             <ComboboxInput
+              ref={inputRef}
+              placeholder={`${placeholder ?? "Select an option"}`}
               className={clsx(
-                'w-full rounded-lg border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white',
-                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                className,
+
+                'w-full bg-white text-sm/6 text-darkBlueFont !mt-2 focus:border-primary focus:ring-0 shadow',
+
+                'relative block w-full appearance-none rounded-xl py-[calc(theme(spacing[2.5])-1px)] sm:py-[calc(theme(spacing[3])-1px)]',
+
+                // Horizontal padding
+                'pl-[calc(theme(spacing[3.5])-1px)] pr-[calc(theme(spacing.7)-1px)] sm:pl-[calc(theme(spacing.3)-1px)]',
+
+                // Typography
+                'text-left text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white forced-colors:text-[CanvasText]',
+
+                // Border
+                'border border-zinc-950/20 group-active:border-zinc-950/30 group-hover:border-zinc-950/30 dark:border-white/10 dark:group-data-[active]:border-white/20 dark:group-data-[hover]:border-white/20',
+
+                // Background color
+                'bg-transparent dark:bg-white/5',
+
+                // Invalid state
+                'group-data-[invalid]:border-red-500 group-data-[invalid]:group-data-[hover]:border-red-500 group-data-[invalid]:dark:border-red-600 group-data-[invalid]:data-[hover]:dark:border-red-600',
+
+                // Disabled state
+                'group-data-[disabled]:border-zinc-950/20 group-data-[disabled]:opacity-100 group-data-[disabled]:dark:border-white/15 group-data-[disabled]:dark:bg-white/[2.5%] dark:data-[hover]:group-data-[disabled]:border-white/15',
               )}
-              displayValue={(option: Option) => option?.name}
+              displayValue={(option: ComboBoxOption) => option?.name}
               onChange={(event) => setQuery(event.target.value)}
             />
-            <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
-              <ChevronDownIcon className="size-4 fill-white/60 group-data-[hover]:fill-white" />
+            <ComboboxButton onClick={changeWidthDropdown} className="group absolute inset-y-0 right-0 px-2.5">
+              <ChevronDownIcon className="size-4 fill-black/60 group-data-[hover]:fill-black" />
             </ComboboxButton>
           </div>
           <ComboboxOptions
+            ref={dropdownRef}
             anchor={{to: "bottom"}}
             style={{width: `${width}px`}}
             className={clsx(
-              '!w-[var(--combobox-dropdown-width)] rounded-lg border border-white/5  p-1 [--anchor-gap:var(--spacing-1)] empty:invisible',
-              'transition duration-100 ease-in bg-black',
+              'rounded-xl border border-white/5 bg-white p-1 [--anchor-gap:var(--spacing-1)] empty:invisible z-50 shadow-lg',
+              'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0',
+              // Border
+              'border border-zinc-950/10 group-active:border-zinc-950/20 group-hover:border-zinc-950/20 dark:border-white/10 dark:group-data-[active]:border-white/20 dark:group-data-[hover]:border-white/20',
               className
             )}
           >
@@ -75,10 +106,11 @@ export function Combobox<T>({ options, value, onChange, className, ...props }: C
               <ComboboxOption
                 key={option.id}
                 value={option}
-                className="group flex items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]: cursor-pointer hover:bg-white hover:bg-opacity-20"
+                className={clsx("group flex items-center hover:!bg-primary gap-2 rounded-lg hover:!text-white py-1.5 px-3 select-none  data-[focus]:bg-white/10 cursor-pointer",
+                )}
               >
-                <CheckIcon className="invisible size-4 fill-white group-data-[selected]:visible" />
-                <div className="text-sm/6 text-white">{option.name}</div>
+                <CheckIcon className="invisible size-4 fill-darkBlueFont group-hover:fill-white group-data-[selected]:visible" />
+                <div className="text-sm/6 text-darkBlueFont group-hover:text-white">{option.name}</div>
               </ComboboxOption>
             ))}
           </ComboboxOptions>
