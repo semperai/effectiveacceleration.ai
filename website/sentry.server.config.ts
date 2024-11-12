47731b899f1501e69dc7c49cbda9d4556d99024d
Kasumi Null <kasumi.null@yandex.com>
2024-11-12 10:40:13 +0800

    import sentry

---
+++ website/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://50776e8503691e4168edcbaaa390fc71@o4508282961657856.ingest.us.sentry.io/4508282963427328",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
