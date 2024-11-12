6bb5f2ee56d79f713172c747ceb90286fd7743c4
jojedev <jorgevillacreces1@gmail.com>
2024-11-11 23:20:03 -0400

    Usdt Default and remove dashboard

--- website/src/app/dashboard/page.tsx
+++ website/src/app/dashboard/page.tsx
@@ -1,11 +1,23 @@
+"use client"
 import { Layout } from '@/components/Dashboard/Layout'
 import React from 'react'
 import DashboardTabs from '@/components/Dashboard/JobsTables/DashboardTabs'
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function IndexPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/dashboard/owner-job-list');
+  }, [router]);
+
+
 return (
     <Layout>
-      <h1>Dashboard content</h1>
+      <></>
     </Layout>
   );
 }
+
+
