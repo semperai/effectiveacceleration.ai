6807c58b328e793350e22877a73c477076c05b52
Kasumi Null <kasumi.null@yandex.com>
2024-11-12 12:38:35 +0800

    remove theme code from logo

--- website/src/components/Logo.tsx
+++ website/src/components/Logo.tsx
@@ -1,21 +1,10 @@
 import Image from 'next/image'
-import { useTheme } from 'next-themes'
-
-import logoLight from '@/images/logo-light.png'
-import logoDark from '@/images/logo-dark.png'
 import logoWhite from '@/images/logo-white.svg'
 
 export function Logo(props: React.ComponentPropsWithoutRef<'div'>) {
-  let { resolvedTheme } = useTheme()
-
   return (
     <div {...props}>
-      {resolvedTheme === 'dark' ? (
-        <Image src={logoWhite} alt="Logo" />
-        ) : (
-        <Image src={logoWhite} alt="Logo" />
-        )
-      }
+      <Image src={logoWhite} alt="Logo" />
     </div>
   )
 }
