7b99ad7538c505a32eda637a1ff6c67ff8de840b
Kasumi Null <kasumi.null@yandex.com>
2024-11-17 15:16:44 +0800

    redirect to home from register page if already registered

--- website/src/app/register/page.tsx
+++ website/src/app/register/page.tsx
@@ -1,20 +1,33 @@
 'use client';
-import React from 'react';
-import PostJobPage from '../dashboard/post-job/PostJobPage';
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation'
+import { useAccount } from 'wagmi';
+import useUser from '@/hooks/useUser';
 import DefaultNavBar from '@/components/DefaultNavBar';
+import PostJobPage from '@/app/dashboard/post-job/PostJobPage';
 import ConnectWallet from './ConnectWallet';
 import SignInMessage from './SignInMessage';
 import CreateProfile from './CreateProfile';
 import ENSConnect from './ENSConnect';
-import { useAccount } from 'wagmi';
-import Image from 'next/image';
 
 export default function RegisterPage() {
-  const { isConnected } = useAccount();
+  const router = useRouter();
+  const { address, isConnected } = useAccount();
+  const { data: user } = useUser(address as `0x${string}`);
   const [encryptionPublicKey, setEncryptionPublicKey] =
     React.useState<`0x${string}`>(`0x`);
   console.log(encryptionPublicKey, 'encryptionPublicKey');
   console.log(isConnected);
+
+  useEffect(() => {
+    if (address && typeof user !== 'undefined') {
+      window.requestAnimationFrame(() => {
+        router.push('/');
+      });
+    }
+  }, [address, user]);
+
   return (
     <>
       <DefaultNavBar />
