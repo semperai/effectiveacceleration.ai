7c56ed20b88bec8794fd2b2fddd6cdc5461fc2dd
Kasumi Null <kasumi.null@yandex.com>
2024-11-17 18:03:59 +0800

    validate both from amount and balance

--- website/src/app/dashboard/post-job/PostJobPage.tsx
+++ website/src/app/dashboard/post-job/PostJobPage.tsx
@@ -459,17 +459,25 @@ const PostJob = forwardRef<{ jobIdCache: (jobId: bigint) => void }, {}>(
         selectedCategory?.name || '',
         { required: true }
       );
-      const paymentTokenValidationMessage = validateField(balanceAsString, {
+
+      const amountValidationMessage = validateField(amount, {
+        mustBeLessThanOrEqualTo: balanceAsString,
+        mustBeGreaterThan: '0',
+        required: true,
+      });
+
+      const paymentTokenValidationMessage = amountValidationMessage || validateField(balanceAsString, {
         mustBeGreaterThanOrEqualTo: amount,
         mustBeGreaterThan: '0',
         required: true,
       });
       console.log(
+        'VALIDATION MESSAGES',
         titleValidationMessage,
         descriptionValidationMessage,
         categoryValidationMessage,
         paymentTokenValidationMessage,
-        'VALIDATION MESSAGES'
+        amountValidationMessage,
       );
       setTitleError(titleValidationMessage);
       setDescriptionError(descriptionValidationMessage);
