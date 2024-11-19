a8daf457aea3b55f065bb5c56bc4a095db96a753
Kasumi Null <kasumi.null@yandex.com>
2024-11-19 11:29:40 +0800

    add favicon

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next';
+import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 import { GoogleAnalytics } from '@next/third-parties/google';
@@ -35,6 +36,14 @@ export default function RootLayout({
       )}
       suppressHydrationWarning
     >
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Effective Acceleration" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className='flex h-full min-h-full flex-col bg-white dark:bg-black'>
         <Providers>{children}</Providers>
         {process.env.NODE_ENV === 'production' && (
