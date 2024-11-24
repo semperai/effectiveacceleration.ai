0e98732aac2bdbb928c0dac4a3d31cff73ebc127
Kasumi Null <kasumi.null@yandex.com>
2024-11-24 16:24:49 +0800

    simplify config

--- contract/hardhat.config.arbitrator.ts
+++ contract/hardhat.config.arbitrator.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { configWithPkey } from "./hardhat.config.base";
 
-let PRIVATE_KEY = process.env.WORKER_PRIVATE_KEY;
+let PRIVATE_KEY = process.env.ARBITRATOR_PRIVATE_KEY;
 if (!PRIVATE_KEY) {
   PRIVATE_KEY = "0x" + "0".repeat(64);
 }
