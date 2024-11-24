46aa854ef70f3c2aea3fa00f25b5887e78bc32a0
Kasumi Null <kasumi.null@yandex.com>
2024-11-24 15:05:41 +0800

    show first tag in assign worker / review

--- website/src/components/JobActions/AssignWorkerButton.tsx
+++ website/src/components/JobActions/AssignWorkerButton.tsx
@@ -153,7 +153,6 @@ export function AssignWorkerButton({
                     <span>
                       <b>Tags:</b>{' '}
                       {job?.tags
-                        .slice(1)
                         .map((value, index) => (
                           <span key={index}>{value}</span>
                         ))}
