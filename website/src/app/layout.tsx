577488b424220cb73340fb7a327904b656b5223e
Kasumi Null <kasumi.null@yandex.com>
2024-11-29 07:42:35 +0800

    use url for metadatabase

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     template: APP_TITLE_TEMPLATE,
   },
   description: APP_DESCRIPTION,
-  metadataBase: process.env.NEXT_PUBLIC_SITE_URL || 'https://effectiveacceleration.ai',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://effectiveacceleration.ai'),
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
