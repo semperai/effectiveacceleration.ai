42f9502cba47f72f06e25a92b4e76c9f76b78a20
jojedev <jorgevillacreces1@gmail.com>
2024-12-16 21:37:41 -0400

    Refuse arbitration pop up

--- website/src/components/JobActions/RefuseArbitrationButton.tsx
+++ website/src/components/JobActions/RefuseArbitrationButton.tsx
@@ -7,7 +7,9 @@ import { MARKETPLACE_V1_ABI } from '@effectiveacceleration/contracts/wagmi/Marke
 import { CheckIcon } from '@heroicons/react/20/solid';
 import * as Sentry from '@sentry/nextjs';
 import { Loader2 } from 'lucide-react';
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
 
 export type RefuseArbitrationButtonProps = {
   job: Job;
@@ -41,6 +43,15 @@ export function RefuseArbitrationButton({
       setIsRefusing(false);
     }
   }
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
 
   const buttonText = isRefusing ? 'Refusing...' : 'Refuse';
 
@@ -48,16 +59,69 @@ export function RefuseArbitrationButton({
     <>
       <Button
         disabled={isRefusing}
-        onClick={handleRefuse}
+        onClick={openModal}
         color={'borderlessGray'}
         className={'w-full'}
       >
         {(isRefusing || isConfirming) && (
           <Loader2 className='mr-2 h-4 w-4 animate-spin' />
         )}
-        <CheckIcon className='-ml-0.5 mr-1.5 h-5 w-5' aria-hidden='true' />
         {buttonText}
       </Button>
+    <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as='div' className='relative z-10' onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                  <Dialog.Title
+                    as='h3'
+                    className='mb-2 text-lg font-medium leading-6 text-gray-900'
+                  >
+                    Refuse Arbitration
+                  </Dialog.Title>
+                  <div>
+                  <span>
+                    If you refuse this arbitration, the job will be unassigned and the funds will be returned to the client. <br/> 
+                  </span>
+                  <Button
+                      disabled={isRefusing}
+                      onClick={handleRefuse}
+                      color={'borderlessGray'}
+                      className={'w-full mt-4'}
+                    >
+                      {(isRefusing || isConfirming) && (
+                        <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                      )}
+                      {buttonText}
+                    </Button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 }
