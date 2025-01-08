9ee24f14db72ab6c16d931d878dfe151da8f701c
jojedev <jorgevillacreces1@gmail.com>
2025-01-08 10:10:52 -0400

    remove clog

--- website/src/tokens.ts
+++ website/src/tokens.ts
@@ -69,6 +69,5 @@ export const formatTokenNameAndAmount = (
 };
 
 export const tokenIcon = (tokenId: string) => {
-  console.log('tokenId',  tokensMap[tokenId]?.icon, tokenId);
   return tokensMap[tokenId]?.icon ?? '';
 };
