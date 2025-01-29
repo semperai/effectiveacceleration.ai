2ba0f8bcbee88fbfa95832b8aa812c18d323a998
jojedev <jorgevillacreces1@gmail.com>
2025-01-29 18:30:55 -0400

    fix events profile images on jobs

--- website/src/components/Events/Components/EventProfileImage.tsx
+++ website/src/components/Events/Components/EventProfileImage.tsx
@@ -4,22 +4,25 @@ import Image from 'next/image';
 import { UserIcon } from '@heroicons/react/20/solid';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import { isImageValid } from '@/utils/ImageValidity';
+import useFetchAvatar from '@/hooks/useFetchAvatar';
 
 const EventProfileImage = ({ user }: { user: User | Arbitrator }) => {
   const [isImgValid, setIsImgValid] = useState(true);
-
+    const [sessionKey, setSessionKey] = useState<string>();
+  const avatarUrl = useFetchAvatar(user?.avatar, sessionKey);
+  console.log(avatarUrl, avatarUrl)
   const handleImageError = () => {
     setIsImgValid(false);
   };
 
   return (
     <>
-      {isImgValid && user?.avatar !== ' ' && user?.avatar ? (
+      {isImgValid && avatarUrl ? (
         <div className='relative flex h-10 min-w-10 items-center bg-gray-400 overflow-hidden rounded-full p-2 align-middle'>
           <Image
             className='h-full w-full object-cover'
             fill
-            src={user?.avatar as string | StaticImport}
+            src={avatarUrl as string | StaticImport}
             alt={'Profile picture'}
             onError={handleImageError}
           />
