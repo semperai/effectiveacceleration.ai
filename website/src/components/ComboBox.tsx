c8168646ec958c27b766ccc490ceb5c3266d58eb
jojedev <jorgevillacreces1@gmail.com>
2025-02-12 18:46:46 -0400

    fix indent

--- website/src/components/ComboBox.tsx
+++ website/src/components/ComboBox.tsx
@@ -39,7 +39,7 @@ export function Combobox({ placeholder, value, options, onChange }: ComboboxProp
           className="w-full justify-between rounded-xl"
         >
           {value
-            ? options.find((option) => option.value === value)?.label
+          ? options.find((option) => option.value === value)?.label
             : placeholder}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
