e0f987921ee91cffbde880d3808a0857afe3471b
jojedev <jorgevillacreces1@gmail.com>
2025-03-11 13:15:29 -0400

    adjust z index for asingWorker and updateButton

--- website/src/components/JobActions/UpdateButton.tsx
+++ website/src/components/JobActions/UpdateButton.tsx
@@ -263,7 +263,7 @@ export function UpdateButton({
         Edit Details
       </Button>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as='div' className='relative z-10' onClose={closeModal}>
+        <Dialog as='div' className='relative  z-50' onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter='ease-out duration-300'
