af0086ef7a3aea6edbce3165bed7f827bcd49080
jojedev <jorgevillacreces1@gmail.com>
2025-03-12 08:11:09 -0400

    raise a dispute styles fix

--- website/src/components/JobActions/DisputeButton.tsx
+++ website/src/components/JobActions/DisputeButton.tsx
@@ -88,7 +88,7 @@ export function DisputeButton({
         Raise a Dispute
       </Button>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as='div' className='relative z-10' onClose={closeModal}>
+        <Dialog as='div' className='relative z-50' onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter='ease-out duration-300'
@@ -112,7 +112,7 @@ export function DisputeButton({
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-8 text-left align-middle shadow-xl transition-all'>
                   <div className='flex flex-col gap-7'>
                     <div>
                       <Dialog.Title
