b4552fd579879c3fd4795b59e36d67c14464259e
Kasumi Null <kasumi.null@yandex.com>
2025-05-06 14:49:58 +0800

    register vercel/otel to stop error

--- website/src/instrumentation.ts
+++ website/src/instrumentation.ts
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
+import { registerOTel } from '@vercel/otel'
 
 export async function register() {
+  registerOTel('next-app');
+
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('../sentry.server.config');
   }
