507dff115dbd524cab3da1bdc0b6901f1b2a66f1
Kasumi Null <kasumi.null@yandex.com>
2025-08-09 14:54:45 +0800

    improve register pages

--- website/src/app/register/page.tsx
+++ website/src/app/register/page.tsx
@@ -27,26 +27,26 @@ export default function RegisterPage() {
   return (
     <>
       <DefaultNavBar />
-      <div className='relative mx-auto flex min-h-customHeader flex-col justify-center'>
+      <div className='relative mx-auto flex min-h-customHeader items-center justify-center'>
         <Image
           src={'/backgroundSignIn.webp'}
           fill
           className='absolute z-0 h-full w-full object-cover'
           alt={'Background Sign In'}
-        ></Image>
-        {isConnected ? (
-          <>
-            {encryptionPublicKey !== `0x` ? (
-              <>
+        />
+        <div className='relative z-10 flex w-full items-center justify-center px-4'>
+          {isConnected ? (
+            <>
+              {encryptionPublicKey !== `0x` ? (
                 <CreateProfile encryptionPublicKey={encryptionPublicKey} />
-              </>
-            ) : (
-              <SignInMessage setEncryptionPublicKey={setEncryptionPublicKey} />
-            )}
-          </>
-        ) : (
-          <ConnectWallet />
-        )}
+              ) : (
+                <SignInMessage setEncryptionPublicKey={setEncryptionPublicKey} />
+              )}
+            </>
+          ) : (
+            <ConnectWallet />
+          )}
+        </div>
       </div>
     </>
   );
