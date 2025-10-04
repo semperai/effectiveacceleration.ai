bf8babcc75e2e2c0fb825331ed61a7a62f419e38
Kasumi Null <kasumi.null@yandex.com>
2025-10-05 02:25:56 +0800

    Add solidity coverage config and update jest setup for website
    - Configure .solcover.js to skip specific contracts and directories
    - Migrate jest.config.ts to .js format with commonjs exports
    - Update @types/react dependencies to stable versions

---
+++ contract/.solcover.js
@@ -0,0 +1,12 @@
+module.exports = {
+  skipFiles: [
+    'EACCToken.sol',
+    'MarketplaceV1.sol',
+    'MarketplaceV2.sol',
+    'MockSablierLockup.sol',
+    'TreasurySplit.sol',
+    'unicrow/',
+    'interfaces/',
+    'libraries/',
+  ]
+};
